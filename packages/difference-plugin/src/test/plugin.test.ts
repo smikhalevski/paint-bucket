73cb3190dec8352d7956295041096b87855250d4
Savva Mikhalevski <smikhalevski@gmail.com>
2021-12-28 00:10:27 +0300

    Added difference-plugin tests

--- packages/difference-plugin/src/test/plugin.test.ts
+++ packages/difference-plugin/src/test/plugin.test.ts
@@ -4,7 +4,13 @@ import '../main';
 
 describe('plugin', () => {
 
-  test('creates color by X11 name', (done) => {
-    color({R: 0xFA, G: 0x80 B: 0x72});
+  test('returns delta E2000', () => {
+    expect(color(0xFF_FF_FF).deltaE2000(color(0xFF_FF_FF))).toBe(0);
+    expect(color(0xAA_BB_CC).deltaE2000(color(0x00_FF_00))).toBeCloseTo(42.8567);
+  });
+
+  test('returns JND status', () => {
+    expect(color(0xFF_FF_FF).isJnd(color(0xFF_FE_FF))).toBe(true);
+    expect(color(0xAA_BB_CC).isJnd(color(0x00_FF_00))).toBe(false);
   });
 });
