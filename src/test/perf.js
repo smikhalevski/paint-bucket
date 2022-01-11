f9159ce4cc78c1ab90d9f72e251c6dd97a4d81b1
Savva Mikhalevski <smikhalevski@gmail.com>
2022-01-11 12:59:20 +0300

    Gradient interpolation perf test

--- src/test/perf.js
+++ src/test/perf.js
@@ -6,7 +6,7 @@ const {color} = require('../../lib/index-cjs');
 
 const rgb = [111, 222, 333];
 
-console.log(chalk.inverse(' Create from array '));
+console.log(chalk.inverse(' Create color from components '));
 gc();
 test('tinycolor2  ', () => tinycolor2(rgb), {timeout: 10000});
 gc();
@@ -14,7 +14,7 @@ test('chroma      ', () => chroma(rgb), {timeout: 10000});
 gc();
 test('paint-bucket', () => color(rgb), {timeout: 10000});
 
-console.log('\n' + chalk.inverse(' Parse HEX '));
+console.log('\n' + chalk.inverse(' Parse color from HEX '));
 gc();
 test('tinycolor2  ', () => tinycolor2('#abcdef'), {timeout: 10000});
 gc();
@@ -22,7 +22,7 @@ test('chroma      ', () => chroma('#abcdef'), {timeout: 10000});
 gc();
 test('paint-bucket', () => color('#abcdef'), {timeout: 10000});
 
-console.log('\n' + chalk.inverse(' Parse RGBa '));
+console.log('\n' + chalk.inverse(' Parse color from RGBa '));
 gc();
 test('tinycolor2  ', () => tinycolor2('rgba(128,128,128,0.5)'), {timeout: 10000});
 gc();
@@ -30,7 +30,7 @@ test('chroma      ', () => chroma('rgba(128,128,128,0.5)'), {timeout: 10000});
 gc();
 test('paint-bucket', () => color('rgba(128,128,128,0.5)'), {timeout: 10000});
 
-console.log('\n' + chalk.inverse(' Desaturate '));
+console.log('\n' + chalk.inverse(' Desaturate color '));
 gc();
 test('tinycolor2  ', () => tinycolor2(rgb).desaturate().toRgb(), {timeout: 10000});
 gc();
@@ -38,14 +38,22 @@ test('chroma      ', () => chroma(rgb).desaturate().rgb(), {timeout: 10000});
 gc();
 test('paint-bucket', () => color(rgb).saturation((S) => S * 0.5).rgb(), {timeout: 10000});
 
-console.log('\n' + chalk.inverse(' Spin + Lighten '));
+console.log('\n' + chalk.inverse(' Spin + Lighten color '));
 gc();
 test('tinycolor2  ', () => tinycolor2(rgb).spin(90).lighten().toRgb(), {timeout: 10000});
 gc();
 test('paint-bucket', () => color(rgb).hue((H) => H + 90).lightness((L) => L + 10).rgb(), {timeout: 10000});
 
-console.log('\n' + chalk.inverse(' Gradient '));
+console.log('\n' + chalk.inverse(' Create gradient from HEX '));
 gc();
-test('chroma      ', () => chroma.scale(['#fff', '#000'])(0.7), {timeout: 10000});
+test('chroma      ', () => chroma.scale(['#fff', '#000']), {timeout: 10000});
 gc();
-test('paint-bucket', () => color.gradient(['#fff', '#000']).at(0.7), {timeout: 10000});
+test('paint-bucket', () => color.gradient(['#fff', '#000']), {timeout: 10000});
+
+console.log('\n' + chalk.inverse(' Linear interpolate gradient '));
+gc();
+const chromaGradient = chroma.scale(['#fff', '#000']);
+test('chroma      ', () => chromaGradient(0.7), {timeout: 10000});
+gc();
+const paintBucketGradient = color.gradient(['#fff', '#000']);
+test('paint-bucket', () => paintBucketGradient.at(0.7), {timeout: 10000});
