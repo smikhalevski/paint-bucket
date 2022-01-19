c88ff854ece50d70190ee810d33414b55a8351f1
Savva Mikhalevski <smikhalevski@gmail.com>
2022-01-19 19:06:54 +0300

    Extracted jest.config.js

--- packages/x11-plugin/src/test/plugin.test.ts
+++ packages/x11-plugin/src/test/plugin.test.ts
@@ -1,9 +1,11 @@
-import {Color, color, Rgb} from '@paint-bucket/core';
+import {color, Rgb} from '@paint-bucket/core';
 import '../main';
 
 describe('plugin', () => {
 
   test('creates color by X11 name', () => {
+    expect(color('blue').get(Rgb)).toEqual([0, 0, 1, 1]);
+    expect(color('DARKORANGE').get(Rgb)).toEqual([1, 0.5490196078431373, 0, 1]);
     expect(color('cyan').get(Rgb)).toEqual([0, 1, 1, 1]);
   });
 });
