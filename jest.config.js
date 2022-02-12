f21e71f05497b8a01a0e08dd6f333f0eb876ab55
Savva Mikhalevski <smikhalevski@gmail.com>
2022-02-12 22:51:32 +0300

    Fixed jest module mapping

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   rootDir: process.cwd(),
   preset: 'ts-jest',
   moduleNameMapper: {
+    '^paint-bucket$': path.resolve(__dirname, './packages/paint-bucket'),
     '^@paint-bucket/(.*)$': path.resolve(__dirname, './packages/$1/src/main'),
   },
   globals: {
