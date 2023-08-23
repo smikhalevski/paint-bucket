57e79a2f7876b124544b2afa776c10f270ca47ac
smikhalevski <smikhalevski@gmail.com>
2023-08-24 00:04:00 +0300

    Removed gradient plugin

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,11 +2,10 @@
  * @module paint-bucket/core
  */
 
-import { Color, Gradient } from './core';
+import { Color } from './core';
 
 import enableCSS from './plugin/css';
 import enableDifference from './plugin/difference';
-import enableGradient from './plugin/gradient';
 import enableHSL from './plugin/hsl';
 import enablePalette from './plugin/palette';
 import enableRGB from './plugin/rgb';
@@ -14,7 +13,6 @@ import enableX11 from './plugin/x11';
 
 enableCSS(Color);
 enableDifference(Color);
-enableGradient(Color, Gradient);
 enableHSL(Color);
 enablePalette(Color);
 enableRGB(Color);
@@ -22,7 +20,6 @@ enableX11(Color);
 
 export type * from './plugin/css';
 export type * from './plugin/difference';
-export type * from './plugin/gradient';
 export type * from './plugin/hsl';
 export type * from './plugin/palette';
 export type * from './plugin/rgb';
